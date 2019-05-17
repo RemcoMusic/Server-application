ec5b762a9dcdc00c44ad55fc739a446a7211234c
Magnocube <stefangroenendijk1998@hotmail.com>
2019-05-17 12:29:37 +0200

    implemented udp receiving and netwok class

--- Server-application/robot_connection/robotconnection.cpp
+++ Server-application/robot_connection/robotconnection.cpp
@@ -3,4 +3,64 @@
 RobotConnection::RobotConnection()
 {
     communicationSettings.settingVariables.updateRate=0;
+
+    qDebug() << "udp setup"<<endl;
+    //QNetworkInterface hotspot;
+//    QHostAddress hotspotAddress;
+//    foreach(QNetworkAddressEntry address, hotspot.addressEntries())
+//    {
+//        if(address.ip().protocol()== QAbstractSocket::IPv4Protocol)
+//        {
+//            hotspotAddress=address.ip();
+//        }
+//    }
+
+    //ipAdress = ip;
+
+//    //qDebug()<<hotspot.addressEntries();
+//    qDebug()<<hotspotAddress;
+//    qDebug()<<
+    socket = new QUdpSocket(this);
+    socket->bind(QHostAddress::Any,4210);
+    connect(socket,SIGNAL(readyRead()),this,SLOT(readyRead()));
+}
+
+void RobotConnection::sendUDP(QByteArray byteArray, QString ip, int port)
+{
+
+}
+
+void RobotConnection::readyRead()
+{
+
+    QByteArray Buffer;
+    Buffer.resize(socket->pendingDatagramSize());
+
+    QHostAddress sender;
+    quint16 senderPort;
+
+    socket->readDatagram(Buffer.data(),Buffer.size(),&sender,&senderPort);
+
+    qDebug() << "Message From: " << sender.toString();
+    qDebug() << "Message Port: " << senderPort;
+    qDebug() << "Message: " << Buffer;
+
+    processIP(sender.toString());
+
+}
+
+void RobotConnection::connectionloop()
+{
+ emit done();
+}
+
+void RobotConnection::processIP(QString ip)
+{
+//check if IP is new IP
+    //no?, update voltage in bots
+    //yes?, add it to the list
+    //ask the device to turn on ->make local pointer to the device we just asked
+
+//check if there is a new bot with status connecting detected.
+
 }
--- Server-application/robot_connection/robotconnection.h
+++ Server-application/robot_connection/robotconnection.h
@@ -2,12 +2,30 @@
 #define ROBOTCONNECTION_H
 #include "swarmbotcommunicationsettings.h"
 #include <QObject>
+#include <QUdpSocket>
+#include <QDebug>
+#include <QNetworkInterface.h>
+#include <QList>
 extern SwarmBotCommunicationSettings communicationSettings;
-class RobotConnection
+class RobotConnection : public QObject
 {
-
+    Q_OBJECT
 public:
     RobotConnection();
+    QList<QString> IpList;
+    void sendUDP(QByteArray byteArray,QString ip, int port);
+public slots:
+    void readyRead();
+    void connectionloop();
+signals:
+    void done();
+
+private:
+    QUdpSocket * socket;
+    QString lastRequestedBotIP;
+
+    void processIP(QString ip);
+
 
 };
 
